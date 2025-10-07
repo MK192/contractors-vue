6e469a99fc7f339167a1aefba837e0a93fd446ee
MK192 <milann.92@hotmail.com>
2025-10-07 13:40:18 +0200

    inital project setup

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  App
+
+  <p>dsadas</p>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap");
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: "DM Sans", sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
