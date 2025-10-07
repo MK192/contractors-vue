6e858ae9243bdac3dfa2b63d2d04c4851f534765
MK192 <milann.92@hotmail.com>
2025-10-07 16:15:29 +0200

    BaseBadge component added

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+//general
 import BaseCard from "./components/general/BaseCard.vue";
 import BaseButton from "./components/general/BaseButton.vue";
+import BaseBadge from "./components/general/BaseBadge.vue";
 
 import router from "./router";
 import store from "./store";
@@ -9,6 +11,7 @@ import store from "./store";
 const app = createApp(App);
 app.component("BaseCard", BaseCard);
 app.component("BaseButton", BaseButton);
+app.component("BaseBadge", BaseBadge);
 
 app.use(router);
 app.use(store);
