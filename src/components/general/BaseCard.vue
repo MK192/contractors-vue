5c3915a4cc9b9600574a2d6fbe4532f5254b426c
MK192 <milann.92@hotmail.com>
2025-10-14 13:21:46 +0200

    ContractorsList now show data from database instead test data, tanstack query added for fetching

--- src/components/general/BaseCard.vue
+++ src/components/general/BaseCard.vue
@@ -9,7 +9,7 @@
   border-radius: 12px;
   /* border: 2px solid #424242; */
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.66);
-  color: #d6a606;
+  color: white;
   padding: 1rem;
   margin: 2rem auto;
   max-width: 40rem;
